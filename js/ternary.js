352cf7b0e8203b89215b813a221308cd755ee2a7
Renata <76951689+Amoseta@users.noreply.github.com>
2021-01-18 11:32:02 +0200

    ternary

---
+++ js/ternary.js
@@ -0,0 +1,44 @@
+
+const a = 4;
+const b = 2;
+
+const arLygu = a === b ? true : false;
+
+console.log(arLygu);
+
+
+const aukstai = 2;
+const kadaDaugiaaukstis = 5;
+
+const arDaigiaaukstis = aukstai >= kadaDaugiaaukstis ? 'taip' : 'ne";
+console.log(arDaigiaaukstis);
+
+
+const variklisIjungtas = false;
+
+const arJudam = variklisIjungtas === true ? 'vaziuojam" : 'stovim";
+console.log(arJudam);
+
+
+
+
+let alkioAtsakymas = '';
+const iprociai = 'mesedis';
+
+if (arAlkanas === true) {
+    alkioAtsakymas = 'tai valgom';   
+} else {
+    alkioAtsakymas = 'nu, palaukim...';
+}
+
+console.log(alkioAtsakymas);
+
+const arAlkanas
+
+
+const alkioAtsakymas2 = arAlkanas === true ? iprociai === 'mesedis' ? 'uzkask mesytes' : 'uzkask darzoviu' : 'nu, palaukim';
+console.log(alkioAtsakymas2);
+
+
+
+
