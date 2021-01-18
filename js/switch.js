3557009a95c5c80eec3d7b6d18ff0ec57d3ce72d
Renata <76951689+Amoseta@users.noreply.github.com>
2021-01-18 11:31:53 +0200

    switch

---
+++ js/switch.js
@@ -0,0 +1,102 @@
+const day = 8;
+
+switch(day) {
+    case 1:
+        console.log('Pirmadienis');
+        break;
+
+    case 2:
+        console.log('Antradienis');
+        break;
+    default:
+        console.log('Tokios dienos nera');
+        break;
+}
+
+const day2 = 'ant';
+
+switch(day2) {
+    case 'pir':
+        console.log('Pirmadienis');
+        break;
+
+    case 'ant':
+        console.log('Antradienis');
+        break;
+    default:
+        console.log('Tokios dienos nera');
+        break;
+}
+
+const day = 1;
+
+switch (day3) {
+    case 1:
+        console.log('Darbo diena');
+        break;
+    case 2:
+        console.log('Darbo diena');
+        break;
+    case 3:
+        console.log('Darbo diena');
+        break;
+    case 4:
+        console.log('Darbo diena');
+        break;
+    case 5:
+        console.log('Darbo diena');
+        break;
+    case 6:
+        console.log('Savaitgalis');
+        break;
+    case 7:
+        console.log('Savaitgalis');
+        break;
+    default:
+        console.log('Tokios dienos nera');
+        break;
+}
+
+const day = 1;
+
+switch (day3) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        console.log('Darbo diena');
+        break;
+    case 6:
+    case 7:
+        console.log('Savaitgalis');
+        break;
+
+    default:
+        console.log('Tokios dienos nera');
+        break;
+
+}
+
+console.log('--------------------')
+
+const stotele = 'ziedas NV';
+
+switch (stotele) {
+    case 'ziedas':
+        console.log('ziedas');
+    case 'parko':
+        console.log('parko');
+    case 'geniu':
+        console.log('geniu');
+    case 'leliju':
+        console.log('leliju');
+    case 'ziedas':
+        console.log('ziedas');
+        break;
+    default:
+        console.log('stoteles nera');
+        break;
+
+
+}
