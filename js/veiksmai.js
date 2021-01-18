3d0fe717397064763b08654e21237cd2d5ceac26
Renata <76951689+Amoseta@users.noreply.github.com>
2021-01-18 11:29:14 +0200

    veiksmai 1,2

---
+++ js/veiksmai.js
@@ -0,0 +1,36 @@
+const s1 = 3;
+const s2 = 8;
+const s3 = 1;
+
+const suma = s1 + s2 + s3;
+console.log(suma);
+
+console.log('------------------')
+
+const t1 = 'Labas';
+const t2 = 'rytas';
+const t3 = 'sakau';
+const t4 = 'tau';
+
+const tekstas = t1 + ' ' + t2 + ' ' + t3 + ' ' + t4;
+console.log(tekstas);
+
+const tekstas2 = `${t1} ${t2} ${t3} ${t4}`;
+console.log(tekstas2);
+
+console.log('------------------')
+
+const n = [2, 5, 3, 4, 9];
+const arraySuma = n[0] +  n[1] + n[2] + n[3] + n[4];
+console.log(arraySuma);
+
+const alternuojantiSuma = n[0] -  n[1] + n[2] - n[3] + n[4];
+console.log(alternuojantiSuma);
+
+console.log('------------------')
+
+const w = ['Lithuania', 'morning', 'Good'];
+
+const sentence = w[2] + ', ' + w[1] + ', ' + w[0];
+console.log(sentence);
+
